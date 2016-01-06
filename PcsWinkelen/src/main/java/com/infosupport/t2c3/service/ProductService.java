7e6e0c0e11e4943d0445d06db2a23edb65f3e766
Leon Stam <stoux2@gmail.com>
2016-01-06 16:00:57 +0100

    Added an Abstract Rest Service

--- PcsWinkelen/src/main/java/com/infosupport/t2c3/service/ProductService.java
+++ PcsWinkelen/src/main/java/com/infosupport/t2c3/service/ProductService.java
@@ -1,5 +1,6 @@
 package com.infosupport.t2c3.service;
 
+import com.infosupport.t2c3.data.BasicRepository;
 import com.infosupport.t2c3.domain.products.Category;
 import com.infosupport.t2c3.domain.products.Product;
 import com.infosupport.t2c3.repositories.ProductRepository;
@@ -10,6 +11,7 @@ import java.util.Random;
 import javax.annotation.PostConstruct;
 import javax.ws.rs.Produces;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,20 +21,16 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping(value = "/products")
+@CrossOrigin
 @Produces("application/json")
-public class ShoppingService {
+public class ProductService extends AbsRestService<Product> {
 
     @Autowired
     private ProductRepository productRepo;
 
-    @RequestMapping()
-    public List<Product> getAllProducts() {
-        return productRepo.findAll();
-    }
-
-    @RequestMapping("/{id}")
-    public Product getProductById(@PathVariable("id") Long id) {
-        return productRepo.findOne(id);
+    @Override
+    public BasicRepository<Product> provideRepo() {
+        return productRepo;
     }
 
     @PostConstruct
@@ -50,5 +48,4 @@ public class ShoppingService {
             ));
         }
     }
-
 }
---
+++ PlatformServices/src/main/java/com/infosupport/t2c3/service/AbsRestService.java
@@ -0,0 +1,65 @@
+package com.infosupport.t2c3.service;
+
+import com.infosupport.t2c3.data.BasicRepository;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.ws.rs.Produces;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * An Abstract RestService that contains a couple of
+ *  usefull methods for basic REST usage.
+ *
+ * @param <X> The type provided by this service
+ */
+@CrossOrigin
+@Produces("application/json")
+public abstract class AbsRestService<X> {
+
+    /** The repo that is used for basic REST calls. */
+    private BasicRepository<X> repo;
+
+    /**
+     * Get an entity by it's ID.
+     *
+     * @param id the id
+     * @return the value or null
+     */
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public X getById(@PathVariable("id") final long id) {
+        return repo.findOne(id);
+    }
+
+    /**
+     * Get all entities.
+     *
+     * @return list of entities
+     */
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public List<X> getAll() {
+        return repo.findAll();
+    }
+
+    /**
+     * A PostConstruct call that fills all the values
+     *  in this abstract service.
+     */
+    @PostConstruct
+    public void fillValues() {
+        this.repo = provideRepo();
+    }
+
+    /**
+     * Provide a repo for this rest service.
+     *
+     * This is called after construction through
+     *  the post construct #fillValues().
+     * @return the repo
+     */
+    public abstract BasicRepository<X> provideRepo();
+
+}
