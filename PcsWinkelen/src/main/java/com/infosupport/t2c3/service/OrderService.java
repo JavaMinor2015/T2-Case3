840f7e093c391013ec2af54441f74e6cb3c042ab
Koen Vervoordeldonk <kmgvervo@avans.nl>
2016-01-19 13:30:45 +0100

    Added order payment field

--- PcsWinkelen/src/main/java/com/infosupport/t2c3/service/OrderService.java
+++ PcsWinkelen/src/main/java/com/infosupport/t2c3/service/OrderService.java
@@ -167,6 +167,7 @@ public class OrderService {
             Order order = new Order(
                     null,
                     OrderStatus.PLACED,
+                    false,
                     items,
                     new CustomerData(
                             "Remco",
